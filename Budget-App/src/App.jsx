caffa598fbeff8544fd151a7ec00cde53b03704f
Salim <salimbabaji@gmail.com>
2023-11-17 11:18:11 +0100

    Budgets now mapped to screen

--- Budget-App/src/App.jsx
+++ Budget-App/src/App.jsx
@@ -1,32 +1,58 @@
 import { Container } from "react-bootstrap";
 import { Stack, Button } from "react-bootstrap";
 import BudgetCard from "./components/BudgetCard";
+import AddBudgetModal from "./components/AddBudgetModal";
+import { useState } from "react";
+import { useBudgets } from "./context/BudgetsContext";
 
 function App() {
-  return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary"> Add Budget</Button>
-        <Button variant="outline-primary"> Add Expense</Button>
-      </Stack>
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const { budgets, getBudgetExpenses } = useBudgets();
 
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-          gap: "1rem",
-          alignItems: "flex-start",
-        }}
-      >
-        <BudgetCard
-          name="Entertainment"
-          amount={1100}
-          max={1000}
-          gray
-        ></BudgetCard>
-      </div>
-    </Container>
+  return (
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            {" "}
+            Add Budget
+          </Button>
+          <Button variant="outline-primary"> Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          {/** The code below essentially maps out each budget from the budget array based on the budget id
+           * The  amount is gotten by iterating over the array of expenses unique to each budget and adding them together.
+           */}
+          {budgets.map((budget) => {
+            const amount = getBudgetExpenses(budget.id).reduce(
+              (total, expense) => total + expense.amount,
+              0
+            )
+            return (
+            <BudgetCard
+              key={budget.id}
+              name={budget.name}
+              amount={amount}
+              max={budget.max}
+              gray
+            />
+            )
+          })}
+        </div>
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+    </>
   );
 }
 
